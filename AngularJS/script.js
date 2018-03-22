258b106e5c09db221b71fe426c13e260b1267529
Craig Cook <ccook87@uwo.ca>
2018-03-22 01:28:21 +0000

    Adding routes and multiple html templates

--- AngularJS/script.js
+++ AngularJS/script.js
@@ -1,8 +1,19 @@
 var module = angular.module('demoApp', []);
-module.controller('MainCtrl', ['$scope', function($scope){  // "$scope" is arbitrary
+module.controller('MainCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){  // "$scope" is arbitrary
     $scope.data = {};
     $scope.disabled = true;
+    $scope.loading = false;
+    $scope.postResult = 0;
+    
     $scope.submit = function() {
-        alert("Form Submitted!!\nEmail: " + $scope.data.email + "\nPassword: " + $scope.data.password);
+        $scope.loading = true;
+        $http.post('/login', $scope.data).success(function(data) {
+            console.log("Form Success!", data);
+            if(data.loggedIn) {
+                $scope.postResult = 1;
+                $location.url('/report');
+            } else $scope.postResult = 2;
+            $scope.loading = false;
+        });
     };
 }]);
