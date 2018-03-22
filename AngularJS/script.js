0960accc46277172b092ad8f9dee4fd687801296
Craig Cook <ccook87@uwo.ca>
2018-03-22 00:40:02 +0000

    Added submit form event

--- AngularJS/script.js
+++ AngularJS/script.js
@@ -1,4 +1,8 @@
-var module = angular.module('main', []);
-module.controller('formCtrl', ['$scope', function($scope){  // "$scope" is arbitrary
+var module = angular.module('demoApp', []);
+module.controller('MainCtrl', ['$scope', function($scope){  // "$scope" is arbitrary
     $scope.data = {};
+    $scope.disabled = true;
+    $scope.submit = function() {
+        alert("Form Submitted!!\nEmail: " + $scope.data.email + "\nPassword: " + $scope.data.password);
+    };
 }]);
